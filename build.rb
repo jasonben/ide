14f7ebfbe4c3ef11fa3493618c40208f86e81a26
jasonben <git@jasonben.com>
2024-09-10 00:37:14 -0500

    fix build script

--- build.rb
+++ build.rb
@@ -4,16 +4,16 @@ module Docker
   class Build
     def initialize
       load_env
-      check_vars('IDE_DOCKER_IMAGE', 'IDE_BASE_IMAGE', 'REPO')
+      check_vars("IDE_DOCKER_IMAGE", "IDE_BASE_IMAGE")
       create_builder
       build
     end
 
     def load_env
-      if File.exist?('.env')
-        File.readlines('.env').each do |line|
-          next if line.strip.empty? || line.start_with?('#')
-          key, value = line.split('=', 2).map(&:strip)
+      if File.exist?(".env")
+        File.readlines(".env").each do |line|
+          next if line.strip.empty? || line.start_with?("#")
+          key, value = line.split("=", 2).map(&:strip)
           ENV[key] = value
         end
       else
@@ -24,7 +24,7 @@ module Docker
 
     def check_vars(*var_names)
       var_names.each do |var_name|
-        if ENV[var_name].nil? || ENV[var_name].empty?
+        if ENV[var_name].to_s.empty?
           puts "#{var_name} is unset. Fix by copying .env.sample to .env"
           exit 1
         end
@@ -32,21 +32,20 @@ module Docker
     end
 
     def create_builder
-      system('docker buildx create --name localbuilder 2>/dev/null || true')
-      system('docker buildx use localbuilder')
+      system("docker buildx create --name localbuilder 2>/dev/null || true")
+      system("docker buildx use localbuilder")
     end
 
     def build
       system([
-        'docker', 'buildx', 'build',
-        '--load',
-        "--build-arg IDE_BASE_IMAGE=#{ENV['IDE_BASE_IMAGE']}",
-        "--build-arg REPO=#{ENV['REPO']}",
-        "-t #{ENV['REPO']}#{ENV['IDE_DOCKER_IMAGE']}",
-        '.'
-      ].join(' '))
+        "docker", "buildx", "build",
+        "--load",
+        "--pull=false",
+        "--build-arg IDE_BASE_IMAGE=#{ENV["IDE_BASE_IMAGE"]}",
+        "--build-arg REPO=#{ENV["REPO"]}",
+        "-t #{ENV["REPO"]}#{ENV["IDE_DOCKER_IMAGE"]}",
+        "."
+      ].join(" "))
     end
   end
 end
-
-Docker::Build.new
